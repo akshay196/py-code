5b7290672d1ba474809b1959739da7092d231103
Akshay Gaikwad <akgaikwad001@gmail.com>
2017-08-02 18:40:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# py-code
+
+My python code
---
+++ guess_number.py
@@ -0,0 +1,30 @@
+#!/usr/share/env python3
+
+# Guess the number
+
+import random
+
+target_number = random.randint(1,100)
+chances = 3
+
+while True:
+    number = int(input("Enter a number from 1 to 100: "))
+    if(chances == 0):
+        print("No more chances left. Game over")
+        break
+    if(number > 100 or number < 1):
+        print("Select number from 1 to 100 only")
+        continue
+    if(target_number == number):
+        print("Correct! Game over")
+        break
+    if(number > target_number):
+        print("Target is lesser than %d" %number)
+        chances -= 1
+    else:
+        print("Target is bigger than %d" %number)
+        chances -= 1
+
+
+
+
