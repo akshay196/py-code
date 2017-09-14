b24acc707ed85ebda4890a368c4d5db32308d1d6
Akshay Gaikwad <akgaikwad001@gmail.com>
2017-09-14 08:09:38 +0530

    new image download code

---
+++ download_image.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+import imghdr
+
+respond = ""
+
+def check_image():
+    global respond
+    
+    if len(sys.argv) < 2:
+        print("\nMention link")
+        exit()
+    
+    respond = requests.get(sys.argv[1])
+
+    image_format = imghdr.what('',respond.content)
+        
+    if (image_format):
+        print("Image verified which is of the type %s" % image_format)
+        download_image()
+    else:
+        print("404 not found")
+
+def download_image():
+    global respond
+    filename = sys.argv[1].split("/")[-1]
+    img_file = open(filename, "wb")
+    print("hang on..Downloading image")
+    #print(respond.encoding)   
+    #print(respond.text)
+    #print(respond.content)
+    img_file.write(respond.content)
+
+    print("Download Complete.")
+    img_file.close()
+
+    
+check_image()
+
+
